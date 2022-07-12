4f86b2a3e36b3d5e743470f4acb072cf7dc9c63b
ak787866 <52346561+ak787866@users.noreply.github.com>
2022-07-12 13:15:57 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from email.policy import default
 import json
 [default]
-aws_access_key = "Awxyz123454587unn7898"
-aws_access_id = "123456765432345"
+#aws_access_key = "Awxyz123454587unn7898"
+#aws_access_id = "123456765432345"
 output = json
 
